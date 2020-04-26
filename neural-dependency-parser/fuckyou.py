b4919b34dd7819185b2c9529f926a14f948d968a
Arya Vohra <arya.vohra04@gmail.com>
2020-04-26 16:46:21 +0800

    Added minimum deployment target=iOS 13

--- neural-dependency-parser/fuckyou.py
+++ neural-dependency-parser/fuckyou.py
@@ -4,7 +4,7 @@ import tfcoreml
 
 graph_def_file = 'model.pbtxt'
 checkpoint_file = 'model.ckpt'
-frozen_model_file = './frozen_model.pb'
+frozen_model_file = 'frozen_model.pb'
 
 # Output nodes. If there're multiple output ops, use comma separated string, e.g. "out1,out2".
 output_node_names = 'output/softmax' 
@@ -30,12 +30,13 @@ Step 2: Call converter
 # A dictionary of input tensors' name and shape (with batch)
 input_tensor_shapes = {"Placeholder:0":[1,18], "Placeholder:1":[1,18], "Placeholder:2":[1,12], "Placeholder_4:0":[1]} # batch size is 1
 # Output CoreML model path
-coreml_model_file = './model.mlmodel'
+coreml_model_file = 'model.mlmodel'
 output_tensor_names = ['output/softmax:0']
 
 
 # Call the converter
 coreml_model = tfcoreml.convert(
+		minimum_ios_deployment_target="13",
         tf_model_path=frozen_model_file, 
         mlmodel_path=coreml_model_file, 
         input_name_shape_dict=input_tensor_shapes,
